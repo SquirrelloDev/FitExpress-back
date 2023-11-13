2c8dc50ce1982a97e108ef07d2326cf3d954254d
SquirrelloDev <olejnike2000@gmail.com>
2023-11-13 18:23:15 +0100

    Add image upload/download for the meals

--- controllers/mealsController.js
+++ controllers/mealsController.js
@@ -1,8 +1,20 @@
 import Meal from '../models/mealsModel.js'
+import fs from "fs";
+import afs from "fs/promises";
+import path from "path";
 export const getMeals = async (req,res,next) => {
     const meals = await Meal.find({})
-    res.status(200)
-    res.json(meals);
+    if(meals){
+        const mealsImage = meals.map((meal) => {
+            let mealPath = path.join('public', 'images', meal.img_path);
+            const data = fs.readFileSync(mealPath, {encoding: 'base64'})
+            return {...meal._doc, imageBuffer: data}
+        })
+        console.log(mealsImage)
+        res.status(200)
+        res.json(mealsImage);
+    }
+
 }
 export const getMealById = async (req,res,next) => {
     const id = req.params.id;
@@ -11,14 +23,22 @@ export const getMealById = async (req,res,next) => {
         res.status(404)
         return  res.json({message: 'Meal does not exist'});
     }
-    res.status(200)
-    res.json(meal)
+    const mealPath = path.join('public', 'images', meal.img_path)
+    fs.readFile(mealPath, 'base64',(err, data) => {
+        if(err){
+            next(err)
+        }
+        res.status(200)
+        res.json({...meal._doc, fileBuffer: data})
+    })
+
 }
 export const createMeal = async (req,res,next) => {
-    const mealData = req.body;
+    const file = req.file;
+    const mealData = JSON.parse(req.body.data)
     const meal = new Meal({
         ...mealData,
-        img_path: mealData.imgPath,
+        img_path: file.originalname,
         tags_id: mealData.tagsId,
         nutrition_values: mealData.nutritionValues
     })
@@ -31,16 +51,35 @@ export const createMeal = async (req,res,next) => {
     res.json({message: 'Meal created!'});
 }
 export const updateMeal = async (req,res,next) => {
+    const file = req.file
+    const id = req.params.id
+    const mealData = JSON.parse(req.body.data)
+    const existingMeal = await Meal.findByIdAndUpdate(id, {
+        ...mealData,
+        img_path: file.originalname,
+        tags_id: mealData.tagsId,
+        nutrition_values: mealData.nutritionValues
+    });
+    res.status(200)
+    res.json({message: 'Meal updated'})
+
+
 
 }
 export const deleteMeal = async (req,res,next) => {
-    //TODO: also delete meal image
     const id = req.params.id
     const deletedMeal = await Meal.findByIdAndDelete(id);
     if(!deletedMeal){
         res.status(404)
         return  res.json({message: 'Meal does not exist'});
     }
+    const mealPath = path.join('public', 'images', deletedMeal.img_path)
+    console.log(mealPath)
+    fs.unlink(mealPath, (err) =>{
+        if(err){
+            throw (err)
+        }
+    })
     res.status(200);
     res.json({message: 'Meal deleted!'})
 }
--- routes/meals.js
+++ routes/meals.js
@@ -1,9 +1,29 @@
 import express from "express";
-import {createMeal, deleteMeal, getMealById, getMeals} from "../controllers/mealsController.js";
+import {createMeal, deleteMeal, getMealById, getMeals, updateMeal} from "../controllers/mealsController.js";
+import multer from "multer";
+const diskStorage = multer.diskStorage({
+    destination: (req,file,cb) => {
+        cb(null, 'public/images')
+    },
+    filename: (req,file,cb) => {
+        cb(null, file.originalname)
+    }
+})
+const fileFilter = (req,file,cb) => {
+    if(file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
+        cb(null, true)
+    }
+    else{
+        cb(null, false)
+    }
+}
+
+const upload = multer({storage: diskStorage, fileFilter: fileFilter})
 const router = express.Router();
 router.get('/', getMeals);
 router.get('/:id', getMealById)
-router.post('/', createMeal);
+router.post('/', upload.single('image'), createMeal);
+router.put('/:id', upload.single('image'), updateMeal)
 router.delete('/:id', deleteMeal)
 
 export { router as mealsRouter }
