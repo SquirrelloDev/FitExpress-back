5de4f073a8f343bc67899111d6f896de9e563179
Emil Olejnik <56551656+SquirrelloDev@users.noreply.github.com>
2023-11-13 19:23:53 +0100

    Revert "Image upload for meals and diets"

--- controllers/mealsController.js
+++ controllers/mealsController.js
@@ -1,20 +1,8 @@
 import Meal from '../models/mealsModel.js'
-import fs from "fs";
-import afs from "fs/promises";
-import path from "path";
 export const getMeals = async (req,res,next) => {
     const meals = await Meal.find({})
-    if(meals){
-        const mealsImage = meals.map((meal) => {
-            let mealPath = path.join('public', 'images', meal.img_path);
-            const data = fs.readFileSync(mealPath, {encoding: 'base64'})
-            return {...meal._doc, imageBuffer: data}
-        })
-        console.log(mealsImage)
-        res.status(200)
-        res.json(mealsImage);
-    }
-
+    res.status(200)
+    res.json(meals);
 }
 export const getMealById = async (req,res,next) => {
     const id = req.params.id;
@@ -23,22 +11,14 @@ export const getMealById = async (req,res,next) => {
         res.status(404)
         return  res.json({message: 'Meal does not exist'});
     }
-    const mealPath = path.join('public', 'images', meal.img_path)
-    fs.readFile(mealPath, 'base64',(err, data) => {
-        if(err){
-            next(err)
-        }
-        res.status(200)
-        res.json({...meal._doc, fileBuffer: data})
-    })
-
+    res.status(200)
+    res.json(meal)
 }
 export const createMeal = async (req,res,next) => {
-    const file = req.file;
-    const mealData = JSON.parse(req.body.data)
+    const mealData = req.body;
     const meal = new Meal({
         ...mealData,
-        img_path: file.originalname,
+        img_path: mealData.imgPath,
         tags_id: mealData.tagsId,
         nutrition_values: mealData.nutritionValues
     })
@@ -51,35 +31,16 @@ export const createMeal = async (req,res,next) => {
     res.json({message: 'Meal created!'});
 }
 export const updateMeal = async (req,res,next) => {
-    const file = req.file
-    const id = req.params.id
-    const mealData = JSON.parse(req.body.data)
-    const existingMeal = await Meal.findByIdAndUpdate(id, {
-        ...mealData,
-        img_path: file.originalname,
-        tags_id: mealData.tagsId,
-        nutrition_values: mealData.nutritionValues
-    });
-    res.status(200)
-    res.json({message: 'Meal updated'})
-
-
 
 }
 export const deleteMeal = async (req,res,next) => {
+    //TODO: also delete meal image
     const id = req.params.id
     const deletedMeal = await Meal.findByIdAndDelete(id);
     if(!deletedMeal){
         res.status(404)
         return  res.json({message: 'Meal does not exist'});
     }
-    const mealPath = path.join('public', 'images', deletedMeal.img_path)
-    console.log(mealPath)
-    fs.unlink(mealPath, (err) =>{
-        if(err){
-            throw (err)
-        }
-    })
     res.status(200);
     res.json({message: 'Meal deleted!'})
 }
