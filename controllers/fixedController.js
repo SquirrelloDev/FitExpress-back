5ef108a62f4a595b00ad4c4121d7303e2f878d2c
SquirrelloDev <olejnike2000@gmail.com>
2023-11-16 18:43:49 +0100

    Modify controller, add routes for fixed days

--- controllers/fixedController.js
+++ controllers/fixedController.js
@@ -1,42 +1,101 @@
 import DayFixed from '../models/fixedModel.js'
 //TODO: Add pagination here
-export const getDays = async (req,res,next) => {
+export const getFixedDays = async (req, res, next) => {
     const fixedDays = await DayFixed.find({})
         .populate({
             path: 'diets',
-        });
+            populate: {
+                path: 'diet_id'
+            }
+        }).populate({
+            path: 'diets',
+            populate: {
+                path: 'meals.morning',
+            }
+        }).populate({
+            path: 'diets',
+            populate: {
+                path: 'meals.lunch',
+            }
+        }).populate({
+            path: 'diets',
+            populate: {
+                path: 'meals.dinner',
+            }
+        }).populate({
+            path: 'diets',
+            populate: {
+                path: 'meals.teatime',
+            }
+        }).populate({
+            path: 'diets',
+            populate: {
+                path: 'meals.supper',
+            }
+        })
+
     res.status(200)
-    res.json(flexiDays);
+    res.json(fixedDays);
 }
-export const getDay = async (req,res,next) => {
+export const getFixedDay = async (req, res, next) => {
     const date = req.query.date;
-    const flexiDay = await DayFixed.findOne({date: date})
-    if(!flexiDay){
+    const fixedDay = await DayFixed.findOne({date: date}).populate({
+        path: 'diets',
+        populate: {
+            path: 'diet_id'
+        }
+    }).populate({
+        path: 'diets',
+        populate: {
+            path: 'meals.morning',
+        }
+    }).populate({
+        path: 'diets',
+        populate: {
+            path: 'meals.lunch',
+        }
+    }).populate({
+        path: 'diets',
+        populate: {
+            path: 'meals.dinner',
+        }
+    }).populate({
+        path: 'diets',
+        populate: {
+            path: 'meals.teatime',
+        }
+    }).populate({
+        path: 'diets',
+        populate: {
+            path: 'meals.supper',
+        }
+    })
+    if (!fixedDay) {
         res.status(404);
         return res.json({message: "Day not found"})
     }
     res.status(200);
-    res.json(flexiDay);
+    res.json(fixedDay);
 }
-export const createDayEntry = async (req,res,next) => {
+export const createFixedDayEntry = async (req, res, next) => {
     const dayData = req.body;
-    const dayFlexi = new DayFixed(dayData)
-    await dayFlexi.save()
+    const dayFixed = new DayFixed(dayData)
+    await dayFixed.save()
     res.status(201);
     res.json({message: 'Day added!'})
 }
-export const updateDayEntry = async (req,res,next) => {
+export const updateFixedDayEntry = async (req, res, next) => {
     const date = req.query.date;
     const dayData = req.body
     const fixedDay = await DayFixed.updateOne({date: date}, dayData)
     res.status(200);
     res.json({message: `Day ${date} updated`})
 }
-//TODO: Add authorization token for this action
-export const deleteDayEntry = async (req,res,next) => {
+//TODO: Check authorization token for this action
+export const deleteFixedDayEntry = async (req, res, next) => {
     const date = req.query.date;
     const deletedDay = await DayFixed.findOneAndDelete({date: date});
-    if(!deletedDay){
+    if (!deletedDay) {
         res.status(404);
         return res.json({message: `Assignment for the date: ${date} does not exist!`})
     }
