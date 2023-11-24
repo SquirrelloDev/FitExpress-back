9fa5a1443a6d146c106e6a922c5c16efb52bbdad
SquirrelloDev <olejnike2000@gmail.com>
2023-11-24 21:14:09 +0100

    Change the dailyMeals to orders object

--- models/dailyOrdersModel.js
+++ models/dailyOrdersModel.js
@@ -1,7 +1,14 @@
 import mongoose, {Schema} from "mongoose";
+
 const dailyOrderSchema = new Schema({
     date: {type: Schema.Types.Date, required: true},
     isAddingLocked: {type: Boolean, required: true, default: false},
-    dailyMeals: {type: [Schema.Types.ObjectId], required: true, ref: 'Meal'},
+    orders: [{
+        user_id: {type: Schema.Types.ObjectId, required:true, ref: 'User'},
+        diet_id: {type: Schema.Types.ObjectId, ref: 'Diet'},
+        order_id: {type: Schema.Types.ObjectId, required: true, ref: 'Order'},
+        selected_meals: {type: [Schema.Types.ObjectId]}
+    }
+    ],
 })
 export default mongoose.model('Daily_order', dailyOrderSchema);
