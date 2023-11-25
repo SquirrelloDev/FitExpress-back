08db6a092bcc436913dab3d6c326196c16046160
SquirrelloDev <olejnike2000@gmail.com>
2023-11-25 19:35:47 +0100

    Add helper function to get next day at midnight; POST route temporary disabled

--- utils/dates.js
+++ utils/dates.js
@@ -3,3 +3,9 @@ export const isWeekend = (date) => {
   const weekDay= dateObj.getDay();
   return (weekDay === 0 || weekDay === 6);
 }
+export const getNextDayMidnight = () => {
+  const currentDate = new Date();
+  currentDate.setDate(currentDate.getDate() + 1)
+  currentDate.setHours(1,0,0, 0);
+  return currentDate
+}
