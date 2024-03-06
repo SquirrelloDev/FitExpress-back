ac6c073b72d15ef45c59f798d0eb8f04a5b4fe31
SquirrelloDev <olejnike2000@gmail.com>
2024-03-06 08:58:38 +0100

    Add preset reminders for the day

--- utils/pushMessage.js
+++ utils/pushMessage.js
@@ -1,7 +1,52 @@
 import webpush from "web-push";
+import Webpush from "../models/webpushModel.js";
+
+export const hCardPayload = JSON.stringify({
+    title: 'Nawodniony?',
+    options: {
+        body: 'Pochwal się, ile wody wypiłeś w tym dniu'
+    }
+})
+export const mealSelectionPayload = JSON.stringify({
+    title: 'Sprawdź posiłki',
+    options: {
+        body: 'Sprawdź posiłki na następny dzień'
+    }
+})
+export const morningReminder = JSON.stringify({
+    title: 'Czas na śniadanie!',
+    options: {
+        body: 'Śniadanie to podstawa każdego dnia'
+    }
+})
+export const lunchReminder = JSON.stringify({
+    title: 'Czas na lunch!',
+    options: {
+        body: 'Zrób sobie przerwę na lunch'
+    }
+})
+export const dinnerReminder = JSON.stringify({
+    title: 'Czas na obiad!',
+    options: {
+        body: 'Największy i najbardziej wartościowy posiłek'
+    }
+})
+export const teatimeReminder = JSON.stringify({
+    title: 'Pora na podwieczorek!',
+    options: {
+        body: 'Czas na przekąskę'
+    }
+})
+export const supperReminder = JSON.stringify({
+    title: 'Czas na kolację!',
+    options: {
+        body: 'To już ostatni posiłek na dzisiaj'
+    }
+})
 
 
-export const sendNotification = (subscriptions, payloadData) => {
+export const sendNotification = async (payloadData) => {
+    const allSubs = await Webpush.find({});
     const options = {
         vapidDetails: {
             subject: 'mailto:push@fitexpress.com',
@@ -9,7 +54,7 @@ export const sendNotification = (subscriptions, payloadData) => {
             privateKey: process.env.VAPID_SEC
         }
     }
-    subscriptions.forEach(sub => {
+    allSubs.forEach(sub => {
         webpush.sendNotification(sub, payloadData, options);
     });
 }
