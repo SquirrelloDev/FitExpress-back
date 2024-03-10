b2134936ea25c3448be0052cc505892a43c7851a
SquirrelloDev <olejnike2000@gmail.com>
2024-03-10 12:50:36 +0100

    Add cron expressions to notify users

--- utils/cronSetup.js
+++ utils/cronSetup.js
@@ -1,11 +1,45 @@
 import cron from "node-cron";
 import {initDay} from "./daysSetup.js";
 import {lockAddingOrders} from "../controllers/dailyOrdersController.js";
+import {deactivateOrders} from "../controllers/ordersController.js";
+import {
+    dinnerReminder,
+    hCardPayload,
+    lunchReminder,
+    mealSelectionPayload,
+    morningReminder,
+    sendNotification, supperReminder, teatimeReminder
+} from "./pushMessage.js";
 const setupCronJobs = () => {
-  cron.schedule("*/2 * * * *", () => {
+  cron.schedule("0 0 * * *", () => {
       lockAddingOrders();
       initDay();
-      console.log('Day initialized!')
-  })
+  });
+  cron.schedule("55 23 * * *", () => {
+      deactivateOrders()
+  });
+//   WEBPUSH
+    cron.schedule("0 15 * * *", () => {
+        sendNotification(hCardPayload)
+    })
+    cron.schedule("0 22 * * *", () => {
+        sendNotification(mealSelectionPayload)
+    })
+//     WBPUSH-MEALS
+    cron.schedule("0 8 * * *", ()=>{
+        sendNotification(morningReminder)
+    })
+    cron.schedule("0 11 * * *", ()=>{
+        sendNotification(lunchReminder)
+    })
+    cron.schedule("0 14 * * *", ()=>{
+        sendNotification(dinnerReminder)
+    })
+    cron.schedule("0 17 * * *", ()=>{
+        sendNotification(teatimeReminder)
+    })
+    cron.schedule("0 20 * * *", ()=>{
+        sendNotification(supperReminder)
+    })
 }
 export default setupCronJobs
