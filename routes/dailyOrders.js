c234b86f72c591edb2c96a65e78213ef955b0133
SquirrelloDev <olejnike2000@gmail.com>
2024-03-10 13:02:26 +0100

    Disable test route

--- routes/dailyOrders.js
+++ routes/dailyOrders.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {addOrderToList, getAllDailyOrders, getDailyOrderByDate} from "../controllers/dailyOrdersController.js";
 import isAuth from "../middleware/isAuth.js";
+// import {initDay} from "../utils/daysSetup.js";
 const router = express.Router();
 router.get('/', isAuth, getAllDailyOrders);
 // router.post('/', initDay);
