dd57e829245ecd8b9c1485ef6639c8b550066012
SquirrelloDev <olejnike2000@gmail.com>
2024-03-16 10:13:24 +0100

    Test on prod

--- routes/dailyOrders.js
+++ routes/dailyOrders.js
@@ -1,10 +1,10 @@
 import express from "express";
 import {addOrderToList, getAllDailyOrders, getDailyOrderByDate} from "../controllers/dailyOrdersController.js";
 import isAuth from "../middleware/isAuth.js";
-// import {initDay} from "../utils/daysSetup.js";
+import {initDay} from "../utils/daysSetup.js";
 const router = express.Router();
 router.get('/', isAuth, getAllDailyOrders);
-// router.post('/', initDay);
+router.post('/', initDay);
 router.get('/date', isAuth, getDailyOrderByDate);
 router.put('/', isAuth, addOrderToList)
 export {router as dailyRouter}
